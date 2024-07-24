7901c24abafc733f8633007b714b94e7c4c42758
Andrew Greene <andrew@defenseunicorns.com>
2024-07-24 16:46:46 -0600

    fix: add delay states

--- src/ui/src/lib/components/cards/StateCard.svelte
+++ src/ui/src/lib/components/cards/StateCard.svelte
@@ -1,16 +1,12 @@
 <script>
 	import { Card } from 'flowbite-svelte';
-	export let states = [
-		{
-			stateOption: ''
-		}
-	];
+	export let list = [''];
 </script>
 
 <div class="m-4 flex flex-wrap justify-center">
-	{#each states as state}
+	{#each list as state}
 		<div class="m-2 min-w-[200px] text-center">
-			<Card color="primary">{state.stateOption}</Card>
+			<Card color="primary">{state}</Card>
 		</div>
 	{/each}
 </div>
--- src/ui/src/routes/+page.svelte
+++ src/ui/src/routes/+page.svelte
@@ -1,64 +1,35 @@
 <script lang="ts">
-	import ClocksCard from '$lib/components/cards/ClocksCard.svelte'
+	import ClocksCard from '$lib/components/cards/ClocksCard.svelte';
 	import StateCard from '$lib/components/cards/StateCard.svelte';
 	import SpeechToTextCard from '$lib/components/cards/SpeechToTextCard.svelte';
 
 	const testimonials = [
 		{
 			title1: 'Event Start',
-			title2: '00:00:00 Z',
-
+			title2: '00:00:00 Z'
 		},
 		{
 			title1: 'Time to next State',
-			title2: '00:00:00 Z',
-
+			title2: '00:00:00 Z'
 		},
 		{
 			title1: 'Running Clock',
-			title2: '00:00:00 Z',
+			title2: '00:00:00 Z'
 		}
-	]
+	];
 
-	const states = [
-		{
-			stateOption: 'Pre Trial Start'
-		},
-		{
-			stateOption: 'Trial Start'
-		},
-		{
-			stateOption: 'In Transit'
-		},
-		{
-			stateOption: 'Mistrial'
-		},
-		{
-			stateOption: 'RTB'
-		},
-		{
-			stateOption: 'Trial End'
-		},
-		// {
-		// 	stateOption: 'Delay Start'
-		// },
-		// {
-		// 	stateOption: 'Delay End'
-		// }
-	]
+	const states = ['Pre Trial Start', 'Trial Start', 'In Transit', 'Mistrial', 'RTB', 'Trial End'];
 
+	const delayStates = ['Delay Start', 'Delay End'];
 </script>
 
 <div>
+	<ClocksCard {testimonials} />
+	<StateCard list={states} />
+	<StateCard list={delayStates} />
+	<SpeechToTextCard />
 
-
-	<ClocksCard {testimonials}/>
-	<StateCard {states}/>
-	<SpeechToTextCard/>
-
-
-
-	<div class="section ">
+	<div class="section">
 		<div class="section-title">Performance Metrics</div>
 		<div class="section-content">[What is most important to the user?]</div>
 	</div>
