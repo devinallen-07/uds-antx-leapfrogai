a91dd0fa263a4e4dc6e34efa80aa14d2a9309efa
americanthinker <americanthinker@gmail.com>
2024-07-23 20:15:16 -0600

    first signed commit

---
+++ src/prompts/options.py
@@ -0,0 +1,29 @@
+from src.enums.states import States
+
+mag_four = {"Options": [States.TRIAL_START.value,
+                        States.DELAY_START.value,
+                        States.MISTRIAL.value,
+                        States.RTB.value]}
+state_options = {
+    States.TRIAL_START.value : 
+        {"Options": 
+            [States.TRIAL_END.value, 
+             States.DELAY_START.value,
+             States.MISTRIAL.value,
+             States.RTB.value]
+        },
+    States.TRIAL_END.value : 
+        mag_four,
+    States.DELAY_START.value : 
+        {"Options": 
+            [States.DELAY_END.value,
+             States.MISTRIAL.value,
+             States.RTB.value]},
+    States.DELAY_END.value : 
+        mag_four,
+    States.MISTRIAL.value : 
+        {"Options": [States.TRIAL_START.value,
+                     States.DELAY_START.value,
+                     States.RTB.value]}
+
+}
