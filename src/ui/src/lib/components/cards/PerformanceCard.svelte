06e89ac9b0fbe8191a56ad18883b9bc3495bc49f
Andrew Greene <andrew@defenseunicorns.com>
2024-07-30 09:40:11 -0700

    update performance metrics ui

--- src/ui/src/lib/components/cards/PerformanceCard.svelte
+++ src/ui/src/lib/components/cards/PerformanceCard.svelte
@@ -1,5 +1,10 @@
-<script>
+<script lang="ts">
 	import { Badge } from 'flowbite-svelte';
+	import { eventStore } from '../../../stores/stateStore';
+
+	function formatNumber(num: number): string {
+		return num.toFixed(1);
+	}
 </script>
 
 <div class="m-4 flex grid grid-cols-2 flex-wrap justify-center text-xl">
@@ -7,22 +12,22 @@
 		<p>Transcription Time (s)</p>
 		<div class="grid grid-cols-2 p-4 indent-10">
 			<span>minimum</span>
-			<Badge class="m-0.5 w-1/2" border>23.1</Badge>
+			<Badge class="m-0.5 w-1/2" border>{formatNumber($eventStore.performanceMetrics.timeToTranscribePerToken.min)}</Badge>
 			<span>maximum</span>
-			<Badge class="m-0.5 w-1/2" border>23.1</Badge>
+			<Badge class="m-0.5 w-1/2" border>{formatNumber($eventStore.performanceMetrics.timeToTranscribePerToken.max)}</Badge>
 			<span>average</span>
-			<Badge class="m-0.5 w-1/2" border>23.1</Badge>
+			<Badge class="m-0.5 w-1/2" border>{formatNumber($eventStore.performanceMetrics.timeToTranscribePerToken.avg)}</Badge>
 		</div>
 	</div>
 	<div class="grid grid-cols-1">
 		<p>Inferencing Time (s)</p>
 		<div class="grid grid-cols-2 p-4 indent-10">
 			<span>minimum</span>
-			<Badge class="m-0.5 w-1/2" border>23.1</Badge>
+			<Badge class="m-0.5 w-1/2" border>{formatNumber($eventStore.performanceMetrics.timeToInference.min)}</Badge>
 			<span>maximum</span>
-			<Badge class="m-0.5 w-1/2" border>23.1</Badge>
+			<Badge class="m-0.5 w-1/2" border>{formatNumber($eventStore.performanceMetrics.timeToInference.max)}</Badge>
 			<span>average</span>
-			<Badge class="m-0.5 w-1/2" border>23.1</Badge>
+			<Badge class="m-0.5 w-1/2" border>{formatNumber($eventStore.performanceMetrics.timeToInference.avg)}</Badge>
 		</div>
 	</div>
 </div>
