cee8ba3a889302485112d3db2fb61ca540621e13
fingermustache <jamesgreene@defenseunicorns.com>
2024-07-30 00:03:38 -0700

    feat:current state glows

--- src/ui/src/lib/components/cards/StateCard.svelte
+++ src/ui/src/lib/components/cards/StateCard.svelte
@@ -1,12 +1,35 @@
 <script>
 	import { Card } from 'flowbite-svelte';
 	export let list = [''];
+	export let isCurrent = true;
 </script>
 
 <div class="m-4 flex flex-wrap justify-center dark:text-white">
 	{#each list as state}
-		<div class="m-2 min-w-[200px] text-center">
+		<div class="m-2 min-w-[200px] text-center {isCurrent ? 'glow-border' : 'glow-border-inactive'} border-transparent rounded-lg">
 			<Card color="primary" class="dark:bg-gray-700">{state}</Card>
 		</div>
 	{/each}
 </div>
+
+<style>
+	@keyframes glow {
+	  0% {
+		box-shadow: 0 0 5px #2494d1, 0 0 10px #2494d1, 0 0 15px #2494d1, 0 0 20px #2494d1;
+	  }
+	  50% {
+		box-shadow: 0 0 10px #2494d1, 0 0 20px #2494d1, 0 0 30px #2494d1, 0 0 40px #2494d1;
+	  }
+	  100% {
+		box-shadow: 0 0 5px #2494d1, 0 0 10px #2494d1, 0 0 15px #2494d1, 0 0 20px #2494d1;
+	  }
+	}
+
+	.glow-border {
+	  animation: glow 1s infinite;
+	}
+
+	.glow-border-inactive {
+	  animation: none;
+	}
+  </style>
